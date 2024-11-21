030cd2841818463f31a1327ad9c36fc2565167a9
Danny Lambert <dlambert150293@gmail.com>
2024-11-21 18:35:34 +0000

    initial commit and push

---
+++ main.js
@@ -0,0 +1,18 @@
+// Add JavaScript interactions or animations here
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Welcome to The Bogey Bros!");
+    alert("Welcome gimps")
+  });
+  
+
+  document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('scroll', () => {
+      const navbar = document.querySelector('.navbar');
+      if (window.scrollY > 50) {
+        navbar.classList.add('bg-dark', 'shadow');
+      } else {
+        navbar.classList.remove('bg-dark', 'shadow');
+      }
+    });
+  });
+  

e53e0bf9c429dfbbdb843c086d2b2e3c76e80aef
Danny Lambert <dlambert150293@gmail.com>
2024-12-08 14:53:41 +0000

    added js and widened records

--- main.js
+++ main.js
@@ -191,7 +191,6 @@ window.addEventListener('scroll', function() {
         });
         
         
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
               $(document).ready(function () {
                 // Add smooth scrolling to all links
                 $("a").on("click", function (event) {
